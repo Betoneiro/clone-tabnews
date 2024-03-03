3937cc80dfcd61cea8410dd172997493e49e4643
Marques Roberto <marquesrrs@gmail.com>
2024-03-03 21:27:44 +0000

    adiciona arquivo editorconfig

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home() {
     return <div><h1>Eu te amo❤️</h1>
                 <p>Obrigado por fazer parte da minha vida!</p>
             </div>
+                
 }
 
 export default Home;
