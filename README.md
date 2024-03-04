d57f50d7fe451277ab263088beb6d3d69645f7d1
Marques Roberto <marquesrrs@gmail.com>
2024-03-04 23:37:33 +0000

    adiciona prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 This is definitely a description.
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return <div><h1>Eu te amo❤️</h1>
-                <p>Obrigado por fazer parte da minha vida!</p>
-            </div>
-                
+  return (
+    <div>
+      <h1>Eu te amo❤️</h1>
+      <p>Obrigado por fazer parte da minha vida!</p>
+    </div>
+  );
 }
 
 export default Home;
